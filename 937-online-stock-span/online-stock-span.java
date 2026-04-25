5e8bab7af012518a9c8272849124ec0df5f12225
srik1234567 <vtu26029@veltech.edu.in>
2026-04-25 18:08:45 +0530

    Time: 29 ms (86.16%) | Memory: 54.5 MB (45.59%) - LeetSync

---
+++ 937-online-stock-span/online-stock-span.java
@@ -0,0 +1,20 @@
+class StockSpanner {
+
+    private Stack<int[]> stack; 
+    // each element: [price, span]
+
+    public StockSpanner() {
+        stack = new Stack<>();
+    }
+
+    public int next(int price) {
+        int span = 1;
+
+        while (!stack.isEmpty() && stack.peek()[0] <= price) {
+            span += stack.pop()[1];
+        }
+
+        stack.push(new int[]{price, span});
+        return span;
+    }
+}
