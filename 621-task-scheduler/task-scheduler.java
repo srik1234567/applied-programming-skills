7d377319a92bfc0b4ebc91310f68a2ab14ee1e30
srik1234567 <vtu26029@veltech.edu.in>
2026-04-25 17:49:19 +0530

    Time: 4 ms (78.10%) | Memory: 47.9 MB (85.38%) - LeetSync

---
+++ 621-task-scheduler/task-scheduler.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] freq = new int[26];
+
+        for (char task : tasks) {
+            freq[task - 'A']++;
+        }
+
+        Arrays.sort(freq);
+
+        int maxFreq = freq[25];
+        int idleSlots = (maxFreq - 1) * n;
+
+        for (int i = 24; i >= 0 && freq[i] > 0; i--) {
+            idleSlots -= Math.min(maxFreq - 1, freq[i]);
+        }
+
+        idleSlots = Math.max(0, idleSlots);
+
+        return tasks.length + idleSlots;
+    }
+}
